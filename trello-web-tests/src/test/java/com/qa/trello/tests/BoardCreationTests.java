912796d01351bfd3b517b9790b6031f6aa9c7214
MarinaDynina <62899808+MarinaDynina@users.noreply.github.com>
2020-04-21 11:40:36 +0300

    added tests Trello

--- trello-web-tests/src/test/java/com/qa/trello/tests/BoardCreationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/BoardCreationTests.java
@@ -3,39 +3,62 @@ package com.qa.trello.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class BoardCreationTests {
     WebDriver wd;
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //implicit expectation
+        wait = new WebDriverWait(wd, 60);
         wd.manage().window().maximize(); //stretch the screen to maximum
+
         wd.navigate().to("https://trello.com/");
     }
 
     @Test
-    public void testBoardCreation()  {
-        click(By.cssSelector("[href='/login']"));
-
-        click(By.name("user"));
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("Dyninamarina3@gmail.com");
-
-
+    public void testBoardCreation() throws InterruptedException {
+        initLogin();
+        fillLoginFormAtlassianAcc("Dyninamarina3@gmail.com", "1qaz2wsx3edcmad");
+        confirmLogin();
 
     }
 
+    private void confirmLogin() {
+        //wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login-submit"))).click(); //waiting for item on page
+        click(By.id("login-submit"));
+    }
+
+    private void fillLoginFormAtlassianAcc(String userEmail, String password) {
+        type(By.name("user"), userEmail);
+        click(By.cssSelector(".button-green"));
+        type(By.className("kmGRCO"), password);
+    }
+
+    private void initLogin() {
+        click(By.cssSelector("[href='/login']"));
+    }
+
+    private void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
     private void click(By locator) {
-        wd.findElement(locator).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator)).click();
     }
 
     @AfterMethod
     public void tearDown() {
-       // wd.quit();
+        // wd.quit();
     }
 }
