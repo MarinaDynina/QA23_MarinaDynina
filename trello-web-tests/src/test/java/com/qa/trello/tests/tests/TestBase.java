97397f5049508f2ec1708b5a03e1a19d626d59e5
MarinaDynina <62899808+MarinaDynina@users.noreply.github.com>
2020-05-18 00:58:54 +0300

    Added dataProvider, Logger

--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
@@ -2,17 +2,35 @@ package com.qa.trello.tests.tests;
 
 import com.qa.trello.tests.framework.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
-//    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp() throws InterruptedException {
         app.init();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m) {
+        logger.info("Start test "+ m.getName());
+    }
+
+    @AfterMethod
+    public void logTestStop(Method m) {
+        logger.info("Stop test "+ m.getName());
+
     }
 
     @AfterSuite
