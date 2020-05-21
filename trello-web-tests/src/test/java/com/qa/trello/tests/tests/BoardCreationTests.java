b5973008e43c231863996929c2047ef237718d3a
MarinaDynina <62899808+MarinaDynina@users.noreply.github.com>
2020-05-21 15:54:59 +0300

    Added dataProvider from file

--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/BoardCreationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/BoardCreationTests.java
@@ -14,44 +14,61 @@ import java.util.List;
 public class BoardCreationTests extends TestBase {
 
 //    @DataProvider
-//    public Iterator<Object[]> validBoards() throws IOException {
-//        List<Object[]> list = new ArrayList<>();
-//        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/boards.csv")));
-//        String line = reader.readLine();
-//        while (line != null) {
-//            String[] split = line.split(";");
-//            list.add(new Object[]{new Board().withNameOfBoard(split[0])});
-//            reader.readLine();
-//        }
+//    public Iterator<Object[]> validBoardEasy() {
+//        List<Object[]> list = new  ArrayList<>();
+//list.add(new Object[]{"aaaaaaaaaaaaa"});
+//list.add(new Object[]{"1111111111111"});
+//list.add(new Object[]{"awsx okpopllki vgyu-jhgfyhjkl"});
 //        return list.iterator();
 //    }
 
-    @DataProvider
-    public Iterator<Object[]> validBoardEasy() {
-        List<Object[]> list = new  ArrayList<>();
-list.add(new Object[]{"aaaaaaaaaaaaa"});
-list.add(new Object[]{"1111111111111"});
-list.add(new Object[]{"awsx okpopllki vgyu-jhgfyhjkl"});
-        return list.iterator();
-    }
-
-        @Test(dataProvider = "validBoardEasy")
-        public void testBoardCreation (String boardName) throws InterruptedException {
-            Board board = new Board().withNameOfBoard(boardName);
-            Thread.sleep(20000);
-            int before = app.getBoard().getBoardsCount();
-            app.getBoard().initBoardCreation();
-            app.getBoard().fillBoardForm(board);
+//        @Test(dataProvider = "validBoardEasy")
+//        public void testBoardCreation (String boardName) throws InterruptedException {
+//            Board board = new Board().withNameOfBoard(boardName);
+//            Thread.sleep(20000);
+//            int before = app.getBoard().getBoardsCount();
+//            app.getBoard().initBoardCreation();
+//            app.getBoard().fillBoardForm(board);
 //                new Board()
 //                .withNameOfBoard("My board" +System.currentTimeMillis())
 //                .withColorsOfBoard("[title='green']"));
 //                .withColorsOfBoard("//div[@class='photo-attribution-component large']/../../..//div[14]"));
-            Thread.sleep(3000);
-            app.getBoard().goToBoardsPageUrl();
-            int after = app.getBoard().getBoardsCount();
+//            Thread.sleep(3000);
+//            app.getBoard().goToBoardsPageUrl();
+//            int after = app.getBoard().getBoardsCount();
 
-            System.out.println("was: " + before + " now: " + after);
-            Assert.assertEquals(after, before + 1);
+//            System.out.println("was: " + before + " now: " + after);
+//            Assert.assertEquals(after, before + 1);
+//        }
+
+    @DataProvider
+    public Iterator<Object[]> validBoards() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/boards.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new Board().withNameOfBoard(split[0])});
+            line = reader.readLine();
         }
+        return list.iterator();
+    }
+    @Test(dataProvider = "validBoards")
+    public void testBoardCreation (Board board) throws InterruptedException {
+        Thread.sleep(20000);
+        int before = app.getBoard().getBoardsCount();
+        app.getBoard().initBoardCreation();
+        app.getBoard().fillBoardForm(board);
+//                new Board()
+//                .withNameOfBoard("My board" +System.currentTimeMillis())
+//                .withColorsOfBoard("[title='green']"));
+//                .withColorsOfBoard("//div[@class='photo-attribution-component large']/../../..//div[14]"));
+        Thread.sleep(3000);
+        app.getBoard().goToBoardsPageUrl();
+        int after = app.getBoard().getBoardsCount();
+
+        System.out.println("was: " + before + " now: " + after);
+        Assert.assertEquals(after, before + 1);
+    }
     }
 
